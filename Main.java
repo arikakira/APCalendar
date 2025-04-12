4b45ab40a13f92ffce0018fd21165ce00042b70d
arikakira <samanthal242@nycstudents.net>
2025-04-12 02:29:06 +0000

    Update

--- Main.java
+++ Main.java
@@ -1,1 +1,7 @@
-
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(APCalendar.numberOfLeapYears(2000, 2025));
+        System.out.println(APCalendar.dayOfWeek(1, 5, 2019));       // test case 1
+        System.out.println(APCalendar.dayOfWeek(1, 10, 2019));      // test case 2
+    }
+}
